03403d8a1e4c0599ed7ba89dfdc9a4801d48a164
Daniel Loyd <dhloyd@icloud.com>
2018-09-27 17:40:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,36 +1,3 @@
 class Dog < ActiveRecord::Base
 
- attr_accessor :name, :breed
-
-
-def self.create
-  self.create
-  self.save
-end
-
-def self.save
-  self.save
-end
-
-def self.update
-  self.update(name, breed)
-end
-
-def self.find_or_create_by
-
-
-end
-
-
-def self.find_by_name
-
-
-end
-
-
-def self.find_by_id
-
-
-end
-
 end
